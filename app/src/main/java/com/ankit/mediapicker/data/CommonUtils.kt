0ede02eab8e529a166392313db8eb5c4a3f4405d
Ankit Mishra <ankitmishra@itoneclick.com>
2023-01-10 00:32:09 +0530

    First Release(10-01-23 at 10:32PM)

---
+++ app/src/main/java/com/ankit/mediapicker/data/CommonUtils.kt
@@ -0,0 +1,16 @@
+package com.ankit.mediapicker.data
+
+/**
+ * Description: This method is used for the purpose of reducing same try catch blocks. Using inline function also saves the memory.
+ * @author Ankit Mishra
+ * @param function Provide function for which the try/catch is required.
+ * @since 08th Jan, 2022
+ * @return Unit
+ */
+inline fun ifThrowsPrintStackTrace(function: () -> Unit) {
+    try {
+        function()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
